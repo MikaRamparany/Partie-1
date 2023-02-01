8c2ee30028d11cd3d5948a66cf24dcdc1a83f466
Mika RAMPARANY <migame.strass@gmail.com>
2023-02-01 16:37:03 +0100

    Ligne 21 : modifié par Foreach pour corriger le code

---
+++ PHP1Exercice11.php
@@ -0,0 +1,23 @@
+<h1> Exercice 11 </h1> 
+
+Initialiser  un  tableau  de  x  marques  de  voitures.  Ecrire  un  algorithme permettant  de  parcourir  ce tableau et d’en afficher le contenu (une marque de voiture par ligne). Il est également demandé d’afficher le nombre de marques de voitures présentes dans le tableau.
+
+Exemple: tableau ➔«Peugeot», «Renault», «BMW», «Mercedes»
+
+<h2> Résultat</h2>
+
+<?php
+
+// VARIABLES 
+
+$marques = ["Mercedes", "BMW", "Peugeot", "Renault"]; // ou fonction "array"
+$Nbmarque = count($marques);
+
+// Résultat
+
+if ($Nbmarque > 0)
+    echo "Il y a $Nbmarque marque(s) de voitures dans le tableau : <br> <br>";
+
+    foreach($marques as $marque) {      // --> FOREACH .. AS : permet d'afficher en liste vertical la variable souhaitée
+    echo "<br> $marque. <br>";
+    }
