c428c272afdf5a1105e3bc90c1e2a3d8a8d4b5b8
Mika RAMPARANY <migame.strass@gmail.com>
2023-02-01 12:31:21 +0100

    Rédaction variables + boucle For + boucle while

---
+++ PHP1Exercice8.php
@@ -0,0 +1,38 @@
+<h1>  Exercice 8</h1>
+
+Ecrire un algorithme qui renvoie la table de multiplication d’un nombre passé en paramètre sous la forme
+
+
+<br> 
+
+</h2> Résultat </h2> 
+<br>
+<br>
+
+<?php
+
+// VARIABLE 1 et 2
+
+$chiffre = 8;
+$prod = 1;
+$nombre = 8;
+$prod = 1;
+$i = 0;
+// Solution 1 
+
+echo"Solution 1 : <br>";
+for($i =0 ; $i<=10; $i++){
+    $prod = $chiffre*$i;
+    echo $chiffre . ' fois ' . $i . ' = ' . $prod . '<br> <br>';
+ 
+ }
+
+ // Solution 2
+echo "Solution 2 : <br>";
+
+
+while($i <= 10){
+    $prod = $nombre*$i;
+    echo $nombre . ' fois ' . $i . ' = ' . $prod . '<br> <br>';
+    $i++;
+}
