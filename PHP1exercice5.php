b6d69e9b49f623c6b3314b1033a80bb77814d984
Mika RAMPARANY <migame.strass@gmail.com>
2023-02-01 10:39:11 +0100

    Insérer les variables à déclarer + echo résultat

---
+++ PHP1exercice5.php
@@ -0,0 +1,18 @@
+<h1>Exercice5</h1>
+Ecrire un algo qui déclare une valeur en francs et qui la convertit en euros. Attention, la valeur générée devra être arrondie à 2 décimales.
+
+<h2> Résultat</h2>
+<?php 
+
+// Liste VARIABLES
+
+$chaineDeCaracteres = "Montant en francs : ";
+$entier = 100;
+$valeurf= francs;
+$valeur€= €;
+$tauxdechange = 0.1524;
+
+// Résultat 
+
+echo $chaineDeCaracteres .$entier ;
+echo $entier . $valeurf = $entier * $tauxdechange . $valeur€;
