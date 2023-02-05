fb4e46e9e94f0e52db6a5d4455525f046ee15e0a
Mika RAMPARANY <migame.strass@gmail.com>
2023-02-05 10:17:16 +0100

    vérification lignes de code

---
+++ PHP1Exercice12.php
@@ -0,0 +1,15 @@
+<h1> Exercice 12 </h1>
+
+A partir d’une fonction personnalisée et à partir d’un tableau de prénoms et de langue associée (tableau  contenant  clé  et  valeur),  dire  bonjour  aux  différentes  personnes  dans  leur  langue respective (français ➔«Salut», anglais ➔«Hello», espagnol ➔«Hola»)
+
+<h2> Résultat</h2>
+
+<?php 
+
+// VARIABLE 
+
+$Personnes = [
+    "Mickael" => "France",
+    "Virgile" => "Espagne",
+    "Marie-Claire" => "Angleterre"
+];
