5e438c9c6936f433c2485408545ff7c705800ce1
Mika <migame.strass@gmail.com>
2023-02-06 15:41:40 +0100

    Rajout note ligne 15

--- PHP1Exercice14.php
+++ PHP1Exercice14.php
@@ -12,7 +12,7 @@ $today = date("Y-m-d");
 $diff = date_diff(date_create($dateOfBirth), date_create($today));
 echo 'Age de la personne '.$diff->format('%y') .'  '.'ans'.' '.$diff->format('%m') .' '.'mois'.' '.$diff->format('%d') .' '.'jour';
 
-// ! pour afficher en ANNEE MOIS JOUR il faut à chaque fois inclure $diif-> format() pour chaque valeur, soit anné, mois et jour. 
+// ! pour afficher en ANNEE MOIS JOUR il faut à chaque fois inclure $diff-> format() pour chaque valeur, soit anné, mois et jour. 
 
 ?> 
 
